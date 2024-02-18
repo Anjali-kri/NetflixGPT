0a0469dbccaec81d495b1aa7d26d8a96fd298acf
Anjali singh <73298495+anjalibobbysingh1@users.noreply.github.com>
2024-02-18 17:37:54 +0530

    call 1st api

--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
+import { DefaultHeaders } from '../Utils/constant'
 
 const Browse = () => {
+
+  const getNOwPlayMovie = async() => {
+    const data = await fetch("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1", DefaultHeaders);
+    const abc = await data.json();
+    // console.log(json);
+  };
+  // console.log(getNOwPlayMovie);
+
+  useEffect(() => {
+    getNOwPlayMovie();
+  }, [])
   return (
     <div>
       <Header />
--- src/Utils/constant.js
+++ src/Utils/constant.js
@@ -1,3 +1,15 @@
 export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
 export const Avatars = "https://avatars.githubusercontent.com/u/73298495?v=4";
+
+
+
+
+     export const DefaultHeaders = {
+        method: "GET",
+        headers: {
+            accept: "application/json",
+            origin: "https://developer.themoviedb.org",
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzAwYzFlMzY2MjY3NjI0YmZhYmM4OTQ0ZjcyMmY2MSIsInN1YiI6IjY1Y2Y0NzczZmRmOGI3MDE0OTY3MTJjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jtGljFjT46p93XCVZEq7DOYTA_3LcExPwml-TNGFtLg",
+        }
+     };

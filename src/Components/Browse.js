cdae7040eae519eba9f649e5e8b395fb37845f0e
Anjali singh <73298495+anjalibobbysingh1@users.noreply.github.com>
2024-02-20 23:23:16 +0530

    setup store and slice for movies list

--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -1,12 +1,17 @@
 import React, { useEffect } from 'react'
 import Header from './Header'
 import { DefaultHeaders } from '../Utils/constant'
+import { useDispatch } from 'react-redux'
+import { addNowPlayingMovies } from '../Utils/movieSlice'
 
 const Browse = () => {
 
+  const dispatch = useDispatch();
+
   const getNOwPlayMovie = async() => {
     const data = await fetch("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1", DefaultHeaders);
-    const abc = await data.json();
+    const json = await data.json();
+    dispatch(addNowPlayingMovies(json.results));
     // console.log(json);
   };
   // console.log(getNOwPlayMovie);
--- src/Utils/appStore.js
+++ src/Utils/appStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "../Utils/userSlice"
+import userReducer from "../Utils/userSlice";
+import movieReducre from "../Utils/movieSlice";
 
 const appStore = configureStore(
     {
         reducer: {
             user: userReducer,
+            movie: movieReducre,
         },
     }
 );

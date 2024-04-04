252a9227f7f16a4d07f5356c14e219e87c9c4d5e
Anjali singh <73298495+anjalibobbysingh1@users.noreply.github.com>
2024-04-05 00:13:01 +0530

    changes in new branch

--- src/Utils/openai.js
+++ src/Utils/openai.js
@@ -1,5 +1,5 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-    apiKey: 'OPEN_AI_KEY', // This is the default and can be omitted
+    apiKey: 'null', // This is the default and can be omitted
   });
